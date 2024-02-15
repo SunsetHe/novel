dccc6d107eaaef103161be7e02c32a40d17bf195
SunsetHe <2622319129@qq.com>
2024-02-15 23:17:32 +0800

    update

--- dictionary.py
+++ dictionary.py
@@ -17,31 +17,31 @@
 #             csv_writer.writerow(['/toimg/data/' + filename, ''])  # 写入文件名和空值
 
 # 更新字典txt
-# import os
-# import csv
-#
-# # 文件夹路径
-# folder_path = 'png'
-# # 原始 CSV 文件路径
-# csv_path = 'output.csv'
-#
-# # 读取已存在的 PNG 文件名
-# existing_files = set()
-# with open(csv_path, 'r', newline='') as csvfile:
-#     csv_reader = csv.reader(csvfile)
-#     next(csv_reader)  # 跳过表头
-#     for row in csv_reader:
-#         key = row[0].split('/')[-1]  # 提取文件名
-#         existing_files.add(key)
-#
-# # 遍历文件夹中的 PNG 文件，将新文件名添加到 CSV 文件中
-# with open(csv_path, 'a', newline='') as csvfile:
-#     csv_writer = csv.writer(csvfile)
-#     for filename in os.listdir(folder_path):
-#         if filename.endswith('.png') and filename not in existing_files:
-#             csv_writer.writerow(['/toimg/data/' + filename, ''])  # 写入新文件名和空值
-#
-# print("已更新 CSV 文件")
+import os
+import csv
+
+# 文件夹路径
+folder_path = 'png'
+# 原始 CSV 文件路径
+csv_path = 'dictionary.csv'
+
+# 读取已存在的 PNG 文件名
+existing_files = set()
+with open(csv_path, 'r', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    next(csv_reader)  # 跳过表头
+    for row in csv_reader:
+        key = row[0].split('/')[-1]  # 提取文件名
+        existing_files.add(key)
+
+# 遍历文件夹中的 PNG 文件，将新文件名添加到 CSV 文件中
+with open(csv_path, 'a', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.png') and filename not in existing_files:
+            csv_writer.writerow(['/toimg/data/' + filename, ''])  # 写入新文件名和空值
+
+print("已更新 CSV 文件")
 
 
 # 重新排序字典txt
@@ -66,5 +66,5 @@
 #             csv_writer.writerow(row)  # 写入数据行
 #
 # # 调用函数重新排列 CSV 文件
-# rearrange_csv('output.csv', 'output_sorted.csv')
+# rearrange_csv('dictionary.csv', 'dictionary_new.csv')
 # print("重新排列完成")
